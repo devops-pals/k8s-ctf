2e7223472bd6ed576d3051e85afdf7de56deafd5
jgpstuart <jgpstuart@gmail.com>
2022-05-02 22:01:54 -0600

    test tfvars

--- terraform/gar.tf
+++ terraform/gar.tf
@@ -6,23 +6,23 @@ resource "google_project_service" "artifactregistry_googleapis_com" {
 resource "google_artifact_registry_repository" "docker_artifact_registry" {
   provider = google-beta
 
-  count = var.docker_registry_enabled ? 1:0
+  count = var.docker_registry_enabled ? 1 : 0
 
-  location = "us-central1"
+  location      = "us-central1"
   repository_id = "docker-repo"
-  description = "Repository for storing docker images"
-  format = "DOCKER"
-  depends_on = [google_project_service.artifactregistry_googleapis_com]
+  description   = "Repository for storing docker images"
+  format        = "DOCKER"
+  depends_on    = [google_project_service.artifactregistry_googleapis_com]
 }
 
 resource "google_artifact_registry_repository" "helm_artifact_registry" {
   provider = google-beta
 
-  count = var.helm_registry_enabled ? 1:0
+  count = var.helm_registry_enabled ? 1 : 0
 
-  location = "us-central1"
+  location      = "us-central1"
   repository_id = "helm-repo"
-  description = "Repository for storing helm charts"
-  format = "DOCKER"
-  depends_on = [google_project_service.artifactregistry_googleapis_com]
+  description   = "Repository for storing helm charts"
+  format        = "DOCKER"
+  depends_on    = [google_project_service.artifactregistry_googleapis_com]
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,14 +1,14 @@
-variable gke_enabled {
-    type = bool
-    default = false
+variable "gke_enabled" {
+  type    = bool
+  default = false
 }
 
 variable "docker_registry_enabled" {
-    type = bool
-    default = false
+  type    = bool
+  default = false
 }
 
 variable "helm_registry_enabled" {
-    type = bool
-    default = false
+  type    = bool
+  default = false
 }
