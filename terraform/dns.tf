5f16a341f03d04e536488f3d5522e2d3f63b63dc
jgpstuart <jgpstuart@gmail.com>
2022-07-20 22:18:28 -0600

    ingress global ip

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -6,21 +6,21 @@ resource "digitalocean_record" "web1" {
   domain = data.digitalocean_domain.default.id
   type   = "A"
   name   = "web1"
-  value  = google_compute_address.ingress_webchalls.address
+  value  = google_compute_global_address.ingress_webchalls.address
 }
 
 resource "digitalocean_record" "web2" {
   domain = data.digitalocean_domain.default.id
   type   = "A"
   name   = "web2"
-  value  = google_compute_address.ingress_webchalls.address
+  value  = google_compute_global_address.ingress_webchalls.address
 }
 
 resource "digitalocean_record" "web3" {
   domain = data.digitalocean_domain.default.id
   type   = "A"
   name   = "web3"
-  value  = google_compute_address.ingress_webchalls.address
+  value  = google_compute_global_address.ingress_webchalls.address
 }
 
 resource "google_compute_global_address" "ingress_webchalls" {
