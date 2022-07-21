d1aeb6878e2c3e6552fc399c08bc406a93f40267
jgpstuart <jgpstuart@gmail.com>
2022-07-20 21:49:13 -0600

    ingress testing

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -3,21 +3,21 @@ data "digitalocean_domain" "default" {
 }
 
 resource "digitalocean_record" "web1" {
-  domain = digitalocean_domain.default.id
+  domain = data.digitalocean_domain.default.id
   type   = "A"
   name   = "web1"
   value  = google_compute_address.ingress_webchalls.address
 }
 
 resource "digitalocean_record" "web2" {
-  domain = digitalocean_domain.default.id
+  domain = data.digitalocean_domain.default.id
   type   = "A"
   name   = "web2"
   value  = google_compute_address.ingress_webchalls.address
 }
 
 resource "digitalocean_record" "web3" {
-  domain = digitalocean_domain.default.id
+  domain = data.digitalocean_domain.default.id
   type   = "A"
   name   = "web3"
   value  = google_compute_address.ingress_webchalls.address
