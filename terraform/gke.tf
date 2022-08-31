77d02b282a0d0f4e33bfdf58ac80e8c45213caed
jgpstuart <jgpstuart@gmail.com>
2022-08-30 21:15:41 -0600

    increase node size

--- terraform/gke.tf
+++ terraform/gke.tf
@@ -21,7 +21,7 @@ resource "google_container_node_pool" "primary_preemptible_nodes" {
 
   node_config {
     preemptible  = true
-    machine_type = "e2-micro"
+    machine_type = "e2-small"
     oauth_scopes = [
       "https://www.googleapis.com/auth/devstorage.read_only"
     ]
