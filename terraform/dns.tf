1266b17d004df2a21dbadc5d086dc9b88b8f4895
jgpstuart <jgpstuart@gmail.com>
2023-01-15 12:05:17 -0700

    create dns zone

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -1,41 +1,54 @@
-data "digitalocean_domain" "default" {
-  name = "jeremystuart.ca"
+# data "digitalocean_domain" "default" {
+#   name = "jeremystuart.ca"
+# }
+
+# resource "digitalocean_record" "web1" {
+#   domain = data.digitalocean_domain.default.id
+#   type   = "A"
+#   name   = "web1"
+#   value  = google_compute_global_address.ingress_webchalls.address
+# }
+
+# resource "digitalocean_record" "web2" {
+#   domain = data.digitalocean_domain.default.id
+#   type   = "A"
+#   name   = "web2"
+#   value  = google_compute_global_address.ingress_webchalls.address
+# }
+
+# resource "digitalocean_record" "web3" {
+#   domain = data.digitalocean_domain.default.id
+#   type   = "A"
+#   name   = "web3"
+#   value  = google_compute_global_address.ingress_webchalls.address
+# }
+
+# resource "digitalocean_record" "ctfd" {
+#   domain = data.digitalocean_domain.default.id
+#   type = "A"
+#   name = "ctfd"
+#   value = google_compute_global_address.ctfd.address
+# }
+
+# resource "google_compute_global_address" "ingress_webchalls" {
+#   name = "ingress-webchalls"
+#   address_type = "EXTERNAL"
+# }
+
+# resource "google_compute_global_address" "ctfd" {
+#   name = "ctfd"
+#   address_type = "EXTERNAL"
+# }
+
+resource "google_dns_managed_zone" "ctf_dns" {
+  name = "ctf-zone"
+  dns_name = "jstuart.ca"
 }
 
-resource "digitalocean_record" "web1" {
-  domain = data.digitalocean_domain.default.id
-  type   = "A"
-  name   = "web1"
-  value  = google_compute_global_address.ingress_webchalls.address
-}
+# resource "google_dns_record_set" "web1" {
+#   name = "web1.${google_dns_managed_zone.ctf_dns.dns_name}"
+#   type = "A"
+#   ttl  = 300
 
-resource "digitalocean_record" "web2" {
-  domain = data.digitalocean_domain.default.id
-  type   = "A"
-  name   = "web2"
-  value  = google_compute_global_address.ingress_webchalls.address
-}
-
-resource "digitalocean_record" "web3" {
-  domain = data.digitalocean_domain.default.id
-  type   = "A"
-  name   = "web3"
-  value  = google_compute_global_address.ingress_webchalls.address
-}
-
-resource "digitalocean_record" "ctfd" {
-  domain = data.digitalocean_domain.default.id
-  type = "A"
-  name = "ctfd"
-  value = google_compute_global_address.ctfd.address
-}
-
-resource "google_compute_global_address" "ingress_webchalls" {
-  name = "ingress-webchalls"
-  address_type = "EXTERNAL"
-}
-
-resource "google_compute_global_address" "ctfd" {
-  name = "ctfd"
-  address_type = "EXTERNAL"
-}
+#   managed_zone = google_dns_managed_zone.ctf_dns.name
+# }
