be0f1829b646d4fde37d03f5a32667fa79521173
jgpstuart <jgpstuart@gmail.com>
2023-01-31 12:25:55 -0700

    correct key for workload

--- terraform/gke.tf
+++ terraform/gke.tf
@@ -17,7 +17,7 @@ resource "google_container_cluster" "primary" {
   }
 
   workload_identity_config {
-    identity_namespace = "${var.project_name}.svc.id.goog"
+    workload_pool = "${var.project_name}.svc.id.goog"
   }
 
 }
