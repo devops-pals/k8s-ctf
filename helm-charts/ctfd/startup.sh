ad80efa5f0733588dd30dcc9f3a7080c255511b6
jgpstuart <jgpstuart@gmail.com>
2023-02-02 15:43:39 -0700

    dns record for ingress

---
+++ helm-charts/ctfd/startup.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+helm upgrade -n ctfd ctfd . --install
--- terraform/dns.tf
+++ terraform/dns.tf
@@ -53,11 +53,19 @@ resource "google_dns_record_set" "web3" {
 #
 ###############################################################################
 
+resource "google_compute_global_address" "ctfd-ingress" {
+  name = "ctfd"
+  address_type = "EXTERNAL"
+  project = var.project_name
+  ip_version = "IPV4"
+}
 
+resource "google_dns_record_set" "web1" {
+  name = "ctfd.${google_dns_managed_zone.ctf_dns.dns_name}"
+  type = "A"
+  ttl  = 300
 
-# resource "google_compute_global_address" "ctfd" {
-#   name = "ctfd"
-#   address_type = "EXTERNAL"
-#   project = var.project_name
-#   ip_version = "IPV4"
-# }
+  managed_zone = google_dns_managed_zone.ctf_dns.name
+
+  rrdatas = [google_compute_global_address.ctfd-ingress.address]
+}
